30a4651dcb71d290d86952ee7324976ffb0082b9
Anemias <carloslajarasanchez@gmail.com>
2026-05-11 18:47:12 +0200

    Añadidos sonidos de puertas

--- AGENTS.md
+++ AGENTS.md
@@ -1,8 +1,4 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: PruebasLoquero
 - Unity version: Unity 6000.3.12f1
-- Active game object:
-  - Name: Door_Basement
-  - Tag: Untagged
-  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/Pusheables/Objects/DoorController.cs
+++ Assets/Scripts/Pusheables/Objects/DoorController.cs
@@ -1,20 +1,27 @@
 using UnityEngine;
 
+[RequireComponent(typeof(WorldAudioSource))]
 public class DoorController : PusheableObject, ISavable<DoorState>
 {
     [SerializeField] private string saveId;
 
+    [Header("Sonido")]
+    [SerializeField] private float soundCooldown = 1.5f; // segundos entre reproducciones
+
     public string SaveId => saveId;
 
     private ISaveService _saveService;
+    private WorldAudioSource _worldAudio;
+    private float _lastSoundTime = -Mathf.Infinity; // permite sonar desde el primer push
 
     private protected override void Awake()
     {
         base.Awake();
         _saveService = AppContainer.Get<ISaveService>();
-            // Seguridad: si no tiene ID, se asigna uno en editor
-    #if UNITY_EDITOR
-            if (string.IsNullOrEmpty(saveId))
+        _worldAudio = GetComponent<WorldAudioSource>();
+        // Seguridad: si no tiene ID, se asigna uno en editor
+#if UNITY_EDITOR
+        if (string.IsNullOrEmpty(saveId))
             {
                 saveId = System.Guid.NewGuid().ToString();
                 UnityEditor.EditorUtility.SetDirty(this);
@@ -25,7 +32,12 @@ public class DoorController : PusheableObject, ISavable<DoorState>
     public override void Push(Vector3 force)
     {
         base.Push(force);
-        // Reproducir sonido de puerta al ser empujada
+
+        if (Time.time - _lastSoundTime >= soundCooldown)
+        {
+            _worldAudio.Play(SoundType.OpenDoor);
+            _lastSoundTime = Time.time;
+        }
     }
 
     public void SaveState()
