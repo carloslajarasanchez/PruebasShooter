759b5f71705ba20b45c14cdb3beafe45d201e829
ElEspi13 <130975665+ElEspi13@users.noreply.github.com>
2026-05-20 00:23:46 +0200

    pausa arreglada

--- Assets/Scripts/UI/Game/DeactivateUI.cs
+++ Assets/Scripts/UI/Game/DeactivateUI.cs
@@ -1,13 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class DeactivateUI : MonoBehaviour
 {
     private IPlayerInput _playerInput;
+    private IEventService _eventService;
 
     private void Awake()
     {
         _playerInput = AppContainer.Get<IPlayerInput>();
+        _eventService = AppContainer.Get<IEventService>();
     }
 
     private void OnEnable()
@@ -16,12 +19,29 @@ public class DeactivateUI : MonoBehaviour
         _playerInput.Actions.Player.Pause.performed += OpenInventory;
         _playerInput.Actions.UI.Inventory.performed += CloseInventory;
         _playerInput.Actions.UI.Pause.performed += CloseInventory;
+        _eventService.Subscribe<OnGameResumed>(OnPauseResumed);
+        _eventService.Subscribe<OnGamePaused>(OnPaused);
+
+    }
+
+    private void OnPaused(OwnEventBase evnt)
+    {
+        _playerInput.Actions.Player.Inventory.performed -= OpenInventory;
+        _playerInput.Actions.UI.Inventory.performed -= CloseInventory;
+        OpenInventory(default);
+    }
+
+    private void OnPauseResumed(OwnEventBase evnt)
+    {
+        CloseInventory(default);
     }
 
     private void OnDisable()
     {
         _playerInput.Actions.Player.Inventory.performed -= OpenInventory;
         _playerInput.Actions.Player.Pause.performed -= OpenInventory;
+        _eventService.Unsubscribe<OnGameResumed>(OnPauseResumed);
+        _eventService.Unsubscribe<OnGamePaused>(OnPaused);
     }
 
     private void OnDestroy()
